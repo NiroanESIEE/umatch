Correct Machine learning Rotation Y and Z

3d_emoji_mapping/EmojiModifier.py
@@ -9,14 +9,14 @@ from PIL import Image
 
 class EmojiModifier(object):
     
-    def __init__(self, filename, mouth, eyes, rotation):
+    def __init__(self, filename, mouth, eyes, rotations):
         
         self.image = 0
         
         emoji = ObjectReader.ObjectReader("3d_object/" + filename + ".obj", "3d_object/" + filename + ".mtl")
 
         display = (500, 500)
-        object_pos = np.array([0.0, -2.5, -9])
+        object_pos = np.array([0.0, -2.5, -11])
         light_pos = np.array([10, 3, -15])
         angle = 0
     
@@ -28,6 +28,8 @@ class EmojiModifier(object):
         pygame.display.set_caption("Nez ro anne")
     
         self.init_open_gl(display, object_pos)
+        glRotatef(rotations[1], 0, 1, 0)
+        glRotatef(rotations[2], 0, 0, 1)
         
         self.refresh_open_gl()
     
@@ -59,8 +61,11 @@ class EmojiModifier(object):
             color = np.array(emoji.materials[material]['Kd'])
             intensity = 0.1
             for face in faces:
+                """
                 normal = emoji.face_normals[face]
                 output_color = (intensity * color * np.dot(normal, new_light_pos)) / 255
+                """
+                output_color = color
                 glColor3fv(output_color)
                 for vertex in emoji.faces[face]:
                     glVertex3fv(emoji.vertices[vertex])

3d_emoji_mapping/ObjectReader.py
@@ -7,11 +7,11 @@ class ObjectReader(object):
         self.faces = []
         self.colors = []
         self.vertex_normals = []
+        self.face_vertex_normals = {}
         self.face_normals = []
         self.materials_faces = {}
         self.materials = {}
         self.vts = []
-
 
         try:
             fileOBJ = open(file_name_obj)
@@ -36,7 +36,7 @@ class ObjectReader(object):
                     string = line.replace("//", "/-1/")
                     face = []
                     vt = []
-                    vn = []
+                    # vn = []
                     i = string.find(" ") + 1
                     faceLine = string.split(" ")
 
@@ -44,13 +44,23 @@ class ObjectReader(object):
                         faceSplit = faceLine[f].split("/")
                         face.append(int(faceSplit[0]) - 1)
                         vt.append(int(faceSplit[1]) - 1)
-                        vn.append(int(faceSplit[2]) - 1)
+                        # vn.append(int(faceSplit[2]) - 1)
+                        self.face_vertex_normals[int(faceSplit[0]) - 1] = int(faceSplit[2]) - 1
 
                     self.faces.append(tuple(face))
                     lenFaces += 1
                     self.vts.append(tuple(vt))
-                    self.vertex_normals.append(tuple(vn))
+
                     self.materials_faces[key].append(lenFaces - 1)
+                elif line[:3] == "vn ":
+                    index1 = line.find(" ") + 1
+                    index2 = line.find(" ", index1 + 1)
+                    index3 = line.find(" ", index2 + 1)
+
+                    vn = (float(line[index1:index2]), float(line[index2:index3]), float(line[index3:-1]))
+                    vn = (round(vn[0], 3), round(vn[1], 3), round(vn[2], 3))
+                    self.vertex_normals.append(vn)
+
 
             fileOBJ.close()
             fileMTL = open(file_name_mtl)
@@ -75,7 +85,7 @@ class ObjectReader(object):
                 vnsList = []
 
                 for vertex in face:
-                    vnsList.append(self.vertex_normals[vertex])
+                    vnsList.append(self.vertex_normals[self.face_vertex_normals[vertex]])
                 vnsList = np.array(vnsList)
 
                 self.face_normals.append(np.linalg.norm(np.mean(vnsList)))